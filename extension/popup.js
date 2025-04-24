2e2567a304255fec90d93f8a35f9b6478df535a2
Evan Lamb <evanlamb848@gmail.com>
2025-04-23 22:26:16 -0400

    chore: improved popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,11 @@
-console.log('This is a popup!');
+async function sayHello() {
+    let [tab] = await chrome.tabs.query({active: true});
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: () => {
+            alert("hello this is a test!!!");
+        }
+    });
+}
+document.getElementById("run-script").addEventListener("click", sayHello);
+//console.log('This is a popup!');
