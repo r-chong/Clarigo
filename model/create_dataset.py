b681ca1d611829c1634d0b4e67af43ea0dd42b64
Reese Chong <dev.reese.chong@gmail.com>
2025-04-24 23:20:29 -0700

    chore: rename dataset creation file

--- model/create_dataset.py
+++ model/create_dataset.py
@@ -60,10 +60,12 @@ def fetch_video_metadata(video_ids):
     for item in resp.get("items", []):
         snip = item["snippet"]
         vids.append({
-            "id": item["id"],
-            "title": snip["title"],
-            "description": snip["description"],
-            "categoryId": snip["categoryId"],
+            "id": item["id"],                       # str
+            "title": snip["title"],                 # str
+            "description": snip["description"],     # str
+            "categoryId": snip["categoryId"],       # str
+            "tags": snip["tags"],                   # list[str]
+            "channelTitle": snip["channelTitle"]   # str
         })
     return vids
 
@@ -74,10 +76,60 @@ def main():
 
     # TODO: extract lists from positive (educational) and negative (non-educational) video_ids.txt files
 
-    ids = ["6dTyOl1fmDo"]
-
+    ids = ["6dTyOl1fmDo",
+            "UN8Vp1uugW4",
+            "f4pQrebE6jc",
+            "2xxziIWmaSA",
+            "LbT1yp6quS8",
+            "MlK6SIjcjE8",
+            "MSq-KGj_cnY",
+            "T4IX36sP_0c",
+            "T4IX36sP_0c",
+            "kVrFjcq0qiw",
+            "PZ7lDrwYdZc",
+            "9Rc_EjPZgB8",
+            "kQ8DGP9p2LY",
+            "1_H7InPMjaY",
+            "lHafBjtTyKQ",
+            "fW8amMCVAJQ",
+            "6Sl7xxisVZo",
+            "W9uKzPFS1CI",
+            "Unzc731iCUY",
+            "QFvqStqPCRU",
+            "QtOumf09MEQ",
+            "qLBImHhCXSw",
+            "Acl6owhFMHQ",
+            "DmEND-q0LyU",
+            "ay15dc2cvm0",
+            "TKYZTbyQQHY",
+            "yDETVPFdbT4",
+            "48HMCy5F6Vg",
+            "Lf_BUdx26XU",
+            "AOqC3L4NQ2c",
+            "6jDSEhU-feI",
+            "cUnNUgsgthk",
+            "qxuPnBJCTM4",
+            "AcRSJEhFZ-s",
+            "g4OBUupicWg",
+            "1Vo2dVyzoYA",
+            "r6Ss6dvGTIw",
+            "Gytu2rI8_lI",
+            "hlMa4IF8OxY",
+            "xa6me8wou_k",
+            "yk6wbvNPZW0",
+            "UCxHB1tzero",
+            "p-0SOWbzUYI",
+            "Qhsf8Ez679Q",
+            "QrxPuk0JefA",
+            "b78W32I69VA",
+            "r5Iy3v1co0A",
+            "pg827uDPFqA",
+            "-5nhdEDGaws",
+            "aSuQ_wF_Rb8",
+        ]
     # pull metadata
 
+    print(len(ids))
     vids = fetch_video_metadata(ids)
     for v in vids:
         # get the category. fallback is "Unknown"
