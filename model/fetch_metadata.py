8a536857db1773e3e1d4535281ce4535234f0268
Reese Chong <dev.reese.chong@gmail.com>
2025-04-24 21:23:35 -0700

    chore: draft fetch_metadata script, delete placeholder files

---
+++ model/fetch_metadata.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables
+
+# ------------------------------------------------
+# category mapping
+# ------------------------------------------------
+# get all possible categories as numbers
+
+# put into dict
+
+# ------------------------------------------------
+# input the actual video titles via txt file
+
+# batch in groups of 50, calling this endpoint: `videos.list?part=snippet`
+
+# we keep:
+# title, description, categoryId
