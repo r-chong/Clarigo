ab09688d2f73fbfb134ed8ef3ff317cf5518d1e5
r-chong <dev.reese.chong@gmail.com>
2025-09-22 19:45:41 -0400

    create rudimentary logic for hiding videos. hardcoded to filter out titles with 'z'

--- src/content.js
+++ src/content.js
@@ -1,1 +1,32 @@
-// content.js
+// filters out videos we don't want
+const filterVideos = () => {
+    const videoElements = document.querySelectorAll('ytd-rich-item-renderer');
+
+    videoElements.forEach((video) => {
+        // variables unused for now. these will be the inputs to the model call
+        // const title = video.querySelector('title');
+        // const channelName = video.querySelector('author');
+
+        if (title.indexOf('z') != -1) {
+            video.classList.add('cg-hide');
+        } else {
+            video.classList.remove('cg-hide');
+        }
+    });
+};
+
+const runOnce = () => {
+    filterVideos();
+}
+
+// debounce to collapse multiple DOM events into one call
+const debounce = (fn, wait = 300) => {
+    let t;
+
+    return (...args) => {
+        clearTimeout();
+        t = setTimeout(() => fn(...args), wait);
+    }
+}
+const observer = new MutationObserver(debounce(runOnce, 350));
+observer.observe(document.body, {childList: true, subtree: true})
