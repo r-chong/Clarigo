8bfc4df6d56689641cb5e1b5b46203f4e4d5d88c
r-chong <dev.reese.chong@gmail.com>
2025-09-22 20:09:48 -0400

    update content script

--- src/content.js
+++ src/content.js
@@ -1,14 +1,15 @@
 // filters out videos we don't want
 const filterVideos = () => {
-    const videoElements = document.querySelectorAll('ytd-rich-item-renderer');
+    const videoElements = document.querySelectorAll('yt-lockup-view-model');
 
     videoElements.forEach((video) => {
         // variables unused for now. these will be the inputs to the model call
-        // const title = video.querySelector('title');
+        const title = video.querySelector('a.yt-lockup-view-model__title span');
         // const channelName = video.querySelector('author');
 
-        if (title.indexOf('z') != -1) {
+        if (title && title.indexOf('z') != -1) {
             video.classList.add('cg-hide');
+            console.log(title)
         } else {
             video.classList.remove('cg-hide');
         }
@@ -19,6 +20,8 @@ const runOnce = () => {
     filterVideos();
 }
 
+runOnce();
+
 // debounce to collapse multiple DOM events into one call
 const debounce = (fn, wait = 300) => {
     let t;
@@ -28,5 +31,10 @@ const debounce = (fn, wait = 300) => {
         t = setTimeout(() => fn(...args), wait);
     }
 }
+
+const onMutations = debounce(() => {
+    runOnce();
+}, 350);
+
 const observer = new MutationObserver(debounce(runOnce, 350));
 observer.observe(document.body, {childList: true, subtree: true})
